0929d6eb7e19681f95524c1ba3470f59868e3ed7
Yumnaraheem <hamidnoorkhattak1@gmail.com>
2026-01-17 19:16:45 +0500

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,13 @@
+#Area of circle
+r=float(input("Enter radius:"))
+AOC=3.14*r*r
+print("Area of circle is:",AOC)
+#Area of square
+side=float(input("Enter side length:"))
+AOS=side*side
+print("Area of square is:",AOS)
+#Area of triangle
+base=float(input("Enter  base if triangle:"))
+height=float(input("Enter height of triangle:"))
+AOT=0.5*base*height
+print("Area of triangle is:",AOT)
